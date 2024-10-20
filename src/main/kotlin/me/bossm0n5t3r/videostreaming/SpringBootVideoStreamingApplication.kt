9e5e9b1147e4ae84df5f17de5ea64f1db97fab7f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-10-20 23:45:00 +0900

    :tada:

---
+++ src/main/kotlin/me/bossm0n5t3r/videostreaming/SpringBootVideoStreamingApplication.kt
@@ -0,0 +1,11 @@
+package me.bossm0n5t3r.videostreaming
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootVideoStreamingApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootVideoStreamingApplication>(*args)
+}
---
+++ src/test/kotlin/me/bossm0n5t3r/videostreaming/SpringBootVideoStreamingApplicationTests.kt
@@ -0,0 +1,11 @@
+package me.bossm0n5t3r.videostreaming
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootVideoStreamingApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
